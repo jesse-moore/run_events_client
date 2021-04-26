2013a80f255328882af2cf8a2f47bd595e179cf6
Jesse <moore.jesse@gmail.com>
2021-04-26 17:16:02 -0500

    updated endpoint

--- build/script.js
+++ build/script.js
@@ -1,40 +1,19 @@
 window.addEventListener('load', async () => {
     const lambdaRes = document.getElementById('lambda-res')
     lambdaRes.innerHTML = 'waiting for response'
-    const rdsRes = document.getElementById('rds-res')
-    rdsRes.innerHTML = 'waiting for response'
     const lambda = await fetchLambda()
-    const rds = await fetchRDS()
     lambdaRes.innerHTML = lambda
-    rdsRes.innerHTML = rds[0].message
 })
 
 const fetchLambda = async () => {
     try {
         const res = await fetch(
-            'https://sweq06ov4b.execute-api.us-east-1.amazonaws.com/default/testFunction',
+            'https://vnmfe4gkp3.execute-api.us-east-1.amazonaws.com/Prod/api',
             {
                 mode: 'cors',
             }
         )
-        const { body } = await res.json()
-        return body
-    } catch (error) {
-        console.log(error)
-        return 'error'
-    }
-}
-
-const fetchRDS = async () => {
-    try {
-        const res = await fetch(
-            'https://sweq06ov4b.execute-api.us-east-1.amazonaws.com/default/graphql-api-dev-hello',
-            {
-                mode: 'cors',
-            }
-        )
-        const body = await res.json()
-        return body
+        return await res.text()
     } catch (error) {
         console.log(error)
         return 'error'
