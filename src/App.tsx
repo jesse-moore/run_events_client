446c45130164369e66919db2ddd9ddfc30cc57ee
Jesse <moore.jesse@gmail.com>
2021-04-28 12:16:12 -0500

    Init typescript react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Home } from './pages/Home'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <Home />
+        </div>
+    )
+}
+
+export default App
