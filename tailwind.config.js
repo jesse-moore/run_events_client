0c025d6654cc1f4fc0ca28b79947a43e8038e850
Jesse <moore.jesse@gmail.com>
2021-04-30 18:40:27 -0500

    configured styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,41 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            blue: colors.blue,
+            gray: colors.trueGray,
+            indigo: colors.indigo,
+            red: colors.red,
+            yellow: colors.amber,
+            blueGray: colors.blueGray,
+            lightBlue: colors.lightBlue,
+            darkBlue: colors.darkBlue,
+            green: colors.green,
+        },
+        extend: {
+            height: {
+                500: '500px',
+                450: '450px',
+                400: '400px',
+                350: '350px',
+            },
+            width: {
+                500: '500px',
+                450: '450px',
+                400: '400px',
+                350: '350px',
+            },
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [],
-}
+    plugins: [require('@tailwindcss/forms')],
+};
